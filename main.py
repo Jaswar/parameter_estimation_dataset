b539ee4b720813f500d37c51391effa565c68a97
Jan Warchocki <janwarchocki@gmail.com>
2024-09-20 23:44:31 +0200

    Save masks to file

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ def get_relevant_videos(videos_path, min_frames=60):
     videos = {}
     for root, dirs, files in os.walk(videos_path):
         for file in files:
-            # if file != 'Camera_1.mp4':
-            #     continue
+            if file != 'Camera_1.mp4':
+                continue
             if not file.endswith('.mp4'):
                 continue
             path = os.path.join(root, file)
@@ -54,7 +54,7 @@ def get_relevant_videos(videos_path, min_frames=60):
 
 def visualize_masks(masks):
     for mask in masks:
-        color = np.array([30 / 255, 144 / 255, 255 / 255])
+        color = np.array([1., 1., 1.])
         h, w = mask.shape[-2:]
         mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
         cv.imshow('mask', mask_image)
@@ -85,6 +85,8 @@ def get_masks_from_video(images_path, predictor, points):
         # propagate the prompts to get masklets throughout the video
         for frame_idx, object_ids, masks in predictor.propagate_in_video(state):
             mask = masks[0].cpu().numpy()
+            mask[mask > 0.] = 255.
+            mask[mask <= 0.] = 0.
             np_masks.append(mask)
     return np_masks
 
@@ -96,23 +98,87 @@ def split_video_into_images(video_path, images_path):
     os.system(f"ffmpeg -i '{video_path}' -q:v 2 -v 2 -start_number 0 {images_path}/'%05d.jpg'")
 
 
-def main():
-    videos_path = 'cvdl_dataset'
-    images_path = 'out'
-    sam2_checkpoint = "models/sam2_hiera_large.pt"
-    model_cfg = "sam2_hiera_l.yaml"
-    device = 'cuda'
+def read_video(video_path):
+    frames = []
+    capture = cv.VideoCapture(video_path)
+    fps = capture.get(cv.CAP_PROP_FPS)
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        frames.append(frame)
+    capture.release()
+    return frames, fps
 
-    predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint, device=device)
-    point_picker = PointPicker(images_path)
-    videos = get_relevant_videos(videos_path)
 
-    for video_path in videos:
+def save_frames(frames, fps, save_path):
+    fourcc = cv.VideoWriter_fourcc(*'MP4V')
+    video_writer = cv.VideoWriter(save_path, fourcc, fps, (frames[0].shape[1], frames[0].shape[0]))
+    for frame in frames:
+        video_writer.write(frame)
+    video_writer.release()
+
+
+def save_video_and_masks(video_path, masks, points, out_path):
+    os.makedirs(out_path)
+
+    masks = np.array(masks, dtype=np.uint8)
+    masks = np.repeat(masks, 3, axis=1)
+    masks = np.transpose(masks, (0, 2, 3, 1))
+
+    frames, fps = read_video(video_path)
+    frames = frames[points[0][0]:]  # the first picked point is the starting point
+    frames = np.array(frames, dtype=np.uint8)
+    assert frames.shape == masks.shape, \
+        f'Shape of masks and frames does not match: {frames.shape} vs {masks.shape}'
+
+    video_save_path = os.path.join(out_path, 'video.mp4')
+    save_frames(frames, fps, video_save_path)
+    masks_save_path = os.path.join(out_path, 'masks.mp4')
+    save_frames(masks, fps, masks_save_path)
+
+
+def process_video(video_path, images_path, predictor, point_picker, out_path):
+    finished = False
+    masks = None
+    points = None
+    while not finished:
         print(f'Processing {video_path}')
         split_video_into_images(video_path, images_path)
         points = point_picker.pick_points()
         masks = get_masks_from_video(images_path, predictor, points)
         visualize_masks(masks)
+
+        choice = input('Repeat? (y/n): ')
+        if choice == 'n':
+            finished = True
+    save_video_and_masks(video_path, masks, points, out_path)
+
+
+def main():
+    videos_path = 'phys101'
+    output_path = 'output'
+    images_path = 'images'
+    sam2_checkpoint = "models/sam2_hiera_large.pt"
+    model_cfg = "sam2_hiera_l.yaml"
+
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint, device=device)
+    point_picker = PointPicker(images_path)
+    videos = get_relevant_videos(videos_path)
+
+    for video_path in videos:
+        video_path_structure = os.path.dirname(video_path).split('/')[1:]
+        out_path = os.path.join(output_path, *video_path_structure)
+        if os.path.exists(out_path):
+            print(f'{video_path} already processed')
+            continue
+
+        process_video(video_path, images_path, predictor, point_picker, out_path)
+
 
 if __name__ == '__main__':
     main()
